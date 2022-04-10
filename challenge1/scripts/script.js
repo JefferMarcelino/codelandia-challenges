b53c6e86233a31c8e9770491698c8073c61d5fb5
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-04-10 16:06:04 +0200

    challenge 1 - part 2 and 3 - completed

---
+++ challenge1/scripts/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.container-post', { delay: 200 });
+ScrollReveal().reveal('.date', { delay: 400 });
+ScrollReveal().reveal('.title', { delay: 500 });
+ScrollReveal().reveal('.description', { delay: 600 });
