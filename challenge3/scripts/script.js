ecd342acfc349ae38d83cd13ff96f405e9588f16
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-04-11 05:47:30 +0200

    challenge 3 completed

---
+++ challenge3/scripts/script.js
@@ -0,0 +1,18 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2500,
+    reset: true
+});
+
+ScrollReveal().reveal('section h1', { delay: 100 });
+ScrollReveal().reveal('section p', { delay: 200 });
+
+ScrollReveal().reveal('section button', { delay: 100 });
+ScrollReveal().reveal('section img', { delay: 300 });
+
+ScrollReveal().reveal('section .color', { delay: 100 });
+ScrollReveal().reveal('section .text', { delay: 400 });
+
+ScrollReveal().reveal('section input', { delay: 600 });
+ScrollReveal().reveal('section textarea', { delay: 600 });
